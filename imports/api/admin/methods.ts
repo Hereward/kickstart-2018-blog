9d02db0616e548d9d8ff13ee78ceedd67ba29bff
Hereward Fenton <hereward@planetonline.com.au>
2018-04-13 13:37:56 +0700

    Add imports/api/admin & update Readme

---
+++ imports/api/admin/methods.ts
@@ -0,0 +1,84 @@
+///<reference path="../../../index.d.ts"/>
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { SimpleSchema } from "meteor/aldeed:simple-schema";
+import { ValidatedMethod } from "meteor/mdg:validated-method";
+import { Auth } from "../auth/publish";
+import { userSessions } from "../sessions/publish";
+import { Profiles } from "../profiles/publish";
+import { Images } from "../images/methods";
+
+const authCheck = (methodName, userId) => {
+  let auth = false;
+  if (userId) {
+    let email = Meteor.user().emails[0].address;
+
+    if (email === Meteor.settings.public.adminEmail) {
+      auth = true;
+    }
+  }
+
+  if (!auth) {
+    console.log(`authCheck (${methodName}) - NOT AUTHORISED FOR ADMIN`);
+    throw new Meteor.Error(
+      `not-authorized for admin [${methodName}]`,
+      "Must be administrator to access this function."
+    );
+  }
+  return auth;
+};
+
+export const deleteUser = new ValidatedMethod({
+  name: "admin.deleteUser",
+  validate: null,
+  run(fields) {
+    authCheck("admin.deleteUser", this.userId);
+    //if (!this.isSimulation) {
+    Meteor.users.remove(fields.id);
+    return true;
+    //}
+  }
+});
+
+export const deleteAllUsers = new ValidatedMethod({
+  name: "admin.deleteAllUsers",
+  validate: null,
+
+  run(fields) {
+    authCheck("admin.deleteAllUsers", this.userId);
+
+    let profileRecord: any;
+    profileRecord = Profiles.findOne({ owner: this.userId });
+    let imageId = profileRecord.image_id;
+
+    //console.log(`deleteAllUsers imageId = [${imageId}]`);
+    let email = Meteor.user().emails[0].address;
+    Meteor.users.remove({ _id: { $ne: this.userId } });
+    Auth.remove({ owner: { $ne: this.userId } });
+    userSessions.remove({ owner: { $ne: this.userId } });
+    Profiles.remove({ owner: { $ne: this.userId } });
+    if (!this.isSimulation) {
+      if (imageId) {
+        //let imagesCursor: any;
+        let imagesCursor = Images.find({ _id: { $ne: imageId } });
+        let imagesCount = imagesCursor.count();
+        let imagesArray = imagesCursor.fetch();
+
+        if (imagesCount) {
+          console.log(`deleteAllUsers - image found! [${imagesCount}]`, imagesArray);
+          Images.remove({ _id: { $ne: imageId } }, function remove(error) {
+            if (error) {
+              console.error("IMAGE File wasn't removed, error: " + error.reason);
+            } else {
+              console.info("IMAGE File successfully removed");
+            }
+          });
+        }
+      }
+    }
+
+    console.log(`admin.deleteAllUsers -DONE!`);
+
+    return true;
+  }
+});
